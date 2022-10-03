f69385300604d8b49e72fb6b93ea5610fa0c0e9b
Orlando E <75282113+ordoc@users.noreply.github.com>
2022-10-03 08:23:33 -0500

    Add files via upload

---
+++ Ejercicios/ejercicio2.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Orlando",
+    44,
+    true,
+    new Date(1978,05,03),
+    {
+        titulo:"El club de las cinco de la mañana",
+        autor:"Robin Sharma",
+        fecha:new Date(2018,12,04),
+        url:"https://www.panamericana.com.co/el-club-de-las-5-de-la-manana-568846/p"
+
+    }
+]
